c44de76d4197fb30668da73770287a75ba5602c5
Chitnis <dchitnis@deloitte.com>
2019-09-25 19:07:47 -0400

    working UI calling API

--- UI/src/app/login/login.component.ts
+++ UI/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   user = 'Deepak';
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.httpClient.get('https://localhost:5001/api/Values/1',)
+    .subscribe( 
+      (data) => this.user = data.Value
+    );
+  }
 }
